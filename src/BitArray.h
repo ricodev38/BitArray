66848c6c7a3a1055de139d1c49ca4ac3439cb674
ricodev38 <eric.vinsonnaud@gmail.com>
2025-10-18 20:50:53 +0200

    File creation

--- src/BitArray.h
+++ src/BitArray.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <Arduino.h>
+
+class BitArray {
+private:
+    uint8_t* data;
+    size_t bitCount;
+    size_t byteCount;
+    size_t index; // index courant du bit
+
+public:
+    BitArray(size_t nBits);
+    ~BitArray();
+    void ClearAll();
+    void Set(size_t n);
+    void Set();
+    void Clear(size_t n);
+    void Clear();
+    void Toggle(size_t n);
+    bool Read(size_t n);
+    size_t GetIndex();
+    void SetIndex(size_t n);
+    void Next();
+    size_t Size();
+};
 
